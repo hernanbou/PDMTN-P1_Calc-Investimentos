5ebcbce9ce827cc4c30d502cc00f80682cccfda5
Hernan Bou <hernan_bou@hotmail.com>
2026-04-17 19:15:41 -0300

    feat/ComponenteExibeDados

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,50 @@
+import { useState } from "react"
 import CapturaDados from "./components/CapturaDados"
+import ExibeDados from "./components/ExibeDados"
 
 
-const calcular = (dados) => {
-  console.log(dados)
-}
 
 function App() {
 
+  const [resultado, setResultado] = useState(null)
+
+  const handleDados = (dados) => {
+    let total = Number(dados.valorInicial)
+    let aporte = Number(dados.aporteMensal)
+    let taxa = Number(dados.juros) / 100
+    let meses = Number(dados.periodo)
+
+    let totalInvestido = Number(dados.valorInicial) + (aporte * meses)
+
+    for (let i = 0; i < meses; i++) {
+      total = total + (total * taxa)
+      total = total + aporte
+    }
+
+    let juros = total - totalInvestido
+
+    let rentabilidade = ((juros / totalInvestido) * 100).toFixed(2)
+
+    const resultadoFinal = {
+      total: total.toFixed(2),
+      totalInvestido: totalInvestido.toFixed(2),
+      juros: juros.toFixed(2),
+      aportes: meses,
+      rentabilidade
+    }
+
+    setResultado(resultadoFinal)
+  }
+
   return (
-    <div>
-      <CapturaDados onCalculo={calcular}/>
-    </div>
+    <main>
+      <div>
+        <CapturaDados onCalculo={handleDados}/>
+      </div>
+      <div>
+        <ExibeDados onDados={resultado}/>
+      </div>
+    </main>
   )
 }
 
